007bae32914fb6281894db0c4c75c67af4a5070e
bassetlife <ruben@rubenreyes.net>
2018-12-06 11:21:54 -0600

    Create GrantAccessSPO.ps1

---
+++ GrantAccessSPO.ps1
@@ -0,0 +1,2 @@
+﻿Connect-SPOService -Url https://ezcorp-admin.sharepoint.com
+Set-SPOUser -Site https://ezcorp.sharepoint.com/sites/ProductionSupport  -LoginName rreyes1@ezcorp.com -IsSiteCollectionAdmin $true -ErrorAction SilentlyContinue
